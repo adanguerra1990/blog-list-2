4ee50b3ae2cb8d899ced28add498a8a9ff83525e
adanguerra1990 <aj90guerra@gmail.com>
2024-12-21 09:03:57 -0400

    Añadir funcionalidad para obtener y crear blogs en el componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import blogServices from './services/blogs'
+import Blog from './components/Blog'
+import BlogForm from './components/BlogForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [blogs, setBlogs] = useState([])
+
+  console.log(blogs)
+
+  useEffect(() => {
+    blogServices.getAll().then(blogs => {
+      setBlogs(blogs)
+    })
+  }, [])
+
+  const addBlog = blogObject => {
+    blogServices.create(blogObject).then(returnedBlog => {
+      setBlogs(blogs.concat(returnedBlog))
+    })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <BlogForm createBlog={addBlog} />
+      <h2>Blogs</h2>
+      {blogs.map(blog => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
   )
 }
 
