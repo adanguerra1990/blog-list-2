5d343553a4a77d2a2f7223b4b5de90bc821a4b64
adanguerra1990 <aj90guerra@gmail.com>
2024-12-27 18:06:02 -0400

    Añadir gestión de token y función para obtener información del usuario en el servicio de blogs

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -2,18 +2,45 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/api/blogs/'
 
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
+const getUser = async userId => {
+  console.log(userId)
+  const config = {
+    headers: { Authorization: token },
+  }
+  const response = await axios.get(
+    `http://localhost:3001/api/users/${userId}`,
+    config
+  )
+  return response.data
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
 const create = async newObject => {
-  const response = await axios.post(baseUrl, newObject)
-  console.log(response.data)
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  console.log(config)
+
+  const response = await axios.post(baseUrl, newObject, config)
+  const user = await getUser(response.data.user)
+  console.log(user)
+  response.data.user = user
   return response.data
 }
 
 export default {
   getAll,
   create,
+  setToken,
 }
