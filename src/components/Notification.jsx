8b61276dd52dc1a3cb9fc6516cc98fbc72d348ce
adanguerra1990 <aj90guerra@gmail.com>
2024-12-27 11:34:24 -0400

    Componente Notification para mostrar mensajes de éxito y error

---
+++ src/components/Notification.jsx
@@ -0,0 +1,10 @@
+const Notification = ({ message, type }) => {
+  if (message === null) {
+    return null
+  }
+
+  const className = type === 'error' ? 'error' : 'success'
+  return <div className={className}>{message}</div>
+}
+
+export default Notification
