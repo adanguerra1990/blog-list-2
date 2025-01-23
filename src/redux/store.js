d74f56343ad32124cdfe851aff9cbbce606ec5f5
adanguerra1990 <aj90guerra@gmail.com>
2025-01-23 16:29:57 -0400

    Añadir reducer de autenticación al store de Redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import notificationReducer from './notificationReducer'
 import blogReducer from './blogReducer'
 import formReducer from './formReducer'
+import authReducer from './authReducer'
 
 const store = configureStore({
   reducer: {
     blogs: blogReducer,
     notification: notificationReducer,
     form: formReducer,
+    auth: authReducer,
   },
 })
 
