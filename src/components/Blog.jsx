8a2e4e98db10c382022642cfc770e49ceff3badf
adanguerra1990 <aj90guerra@gmail.com>
2025-01-23 16:28:55 -0400

    Añadir gestión de "like" y eliminación de blogs utilizando Redux en el componente Blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,32 @@
 import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { likeBlog, removeBlog } from '../redux/blogReducer'
+import { showNotification } from '../redux/notificationReducer'
 
-const Blog = ({ blog, user, handleLike, handleDelete }) => {
+const Blog = ({ blog }) => {
   const [visible, setVisible] = useState(false)
+  const user = useSelector(state => state.auth.user)
+  const dispatch = useDispatch()
+
+  const handleLike = async () => {
+    try {
+      dispatch(likeBlog(blog.id))
+      dispatch(showNotification(`you liked '${blog.title}'`, 'success', 5))
+    } catch (error) {
+      dispatch(showNotification('failed to like blog:', 'error', 5))
+    }
+  }
+
+  const handleDelete = async () => {
+    if (window.confirm(`Delete ${blog.title}`)) {
+      try {
+        dispatch(removeBlog(blog.id))
+        dispatch(showNotification('Blog deleted successfully', 'success', 5))
+      } catch (error) {
+        dispatch(showNotification('Failed to delete blog', 'error', 5))
+      }
+    }
+  }
 
   const toggleVisibility = () => {
     setVisible(!visible)
@@ -18,12 +43,10 @@ const Blog = ({ blog, user, handleLike, handleDelete }) => {
         <div>
           <p>Author: {blog.author}</p>
           <p>Url: {blog.url}</p>
-          <button onClick={() => handleLike(blog.id)}>Like</button>
+          <button onClick={handleLike}>Like</button>
           <span>{blog.likes}</span>
-          {canDelete && (
-            <button onClick={() => handleDelete(blog.id)}>Delete</button>
-          )}
-          <p>Create by: {user.name}</p>
+          {canDelete && <button onClick={handleDelete}>Delete</button>}
+          <p>Create by: {blog.user.name}</p>
         </div>
       )}
       <button onClick={toggleVisibility}>{visible ? 'Hide' : 'Show'}</button>
--- src/redux/blogReducer.js
+++ src/redux/blogReducer.js
@@ -11,10 +11,18 @@ const blogSlice = createSlice({
     addBlog(state, action) {
       state.push(action.payload)
     },
+    updateBlog(state, action) {
+      return state.map(blog =>
+        blog.id !== action.payload.id ? blog : action.payload
+      )
+    },
+    deleteBlog(state, action) {
+      return state.filter(blog => blog.id !== action.payload)
+    },
   },
 })
 
-export const { setBlogs, addBlog } = blogSlice.actions
+export const { setBlogs, addBlog, updateBlog, deleteBlog } = blogSlice.actions
 
 export const initialBlogs = () => {
   return async dispatch => {
@@ -27,6 +35,25 @@ export const createBlog = newBlog => {
   return async dispatch => {
     const blog = await blogServices.create(newBlog)
     dispatch(addBlog(blog))
+  }
+}
+
+export const likeBlog = id => {
+  return async (dispatch, getState) => {
+    const blogToLike = getState().blogs.find(blog => blog.id === id)
+    const updateToBlog = await blogServices.update(id, {
+      ...blogToLike,
+      likes: blogToLike.likes + 1,
+      user: blogToLike.user,
+    })
+    dispatch(updateBlog(updateToBlog))
+  }
+}
+
+export const removeBlog = id => {
+  return async dispatch => {
+    await blogServices.remove(id)
+    dispatch(deleteBlog(id))
   }
 }
 
