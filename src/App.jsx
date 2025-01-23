07e208f4ef90964a4a9993038fcd97aa46621c0a
adanguerra1990 <aj90guerra@gmail.com>
2025-01-23 16:29:45 -0400

    Refactor gestión de usuario en el componente App, eliminando estado local y utilizando Redux para la autenticación

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 import blogServices from './services/blogs'
-import loginServices from './services/login'
 import Blog from './components/Blog'
 import BlogForm from './components/BlogForm'
 import LoginForm from './components/LoginForm'
@@ -9,16 +8,16 @@ import Notification from './components/Notification'
 import Togglable from './components/Togglable'
 import { useDispatch, useSelector } from 'react-redux'
 import { showNotification } from './redux/notificationReducer'
-import { createBlog, initialBlogs } from './redux/blogReducer'
+import { initialBlogs } from './redux/blogReducer'
+import { logoutUser, setUser } from './redux/authReducer'
 
 function App() {
-  const [setBlogs] = useState([])
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null)
+  const user = useSelector(state => state.auth.user)
   const notification = useSelector(state => state.notification)
   const blogs = useSelector(state => state.blogs)
   const dispatch = useDispatch()
+
+  console.log('user', user)
 
   useEffect(() => {
     if (user) {
@@ -30,69 +29,14 @@ function App() {
     const loggedUserJSON = window.localStorage.getItem('loggedBlogListUser')
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
-      setUser(user)
+      dispatch(setUser(user))
       blogServices.setToken(user.token)
     }
-  }, [])
-
-  const handleLogin = async event => {
-    event.preventDefault()
-
-    try {
-      const user = await loginServices.login({ username, password })
-      window.localStorage.setItem('loggedBlogListUser', JSON.stringify(user))
-
-      setUser(user)
-      setPassword('')
-      setUsername('')
-      blogServices.setToken(user.token)
-      dispatch(showNotification(`logged in user: ${user.name}`, 'success', 5))
-    } catch (error) {
-      dispatch(showNotification('Invalid username or password', 'error', 5))
-    }
-  }
+  }, [dispatch])
 
   const handleLogout = () => {
-    setUser(null)
+    dispatch(logoutUser())
     dispatch(showNotification('logged out', 'success', 5))
-    window.localStorage.removeItem('loggedBlogListUser')
-  }
-
-  const handleLike = async id => {
-    const blogToLike = blogs.find(blog => blog.id === id)
-    const updateBlog = {
-      ...blogToLike,
-      likes: blogToLike.likes + 1,
-    }
-
-    try {
-      const returnedBlog = await blogServices.update(id, updateBlog)
-      setBlogs(
-        blogs.map(blog =>
-          blog.id !== id ? blog : { ...returnedBlog, user: blogToLike.user }
-        )
-      )
-
-      dispatch(
-        showNotification(`you liked '${returnedBlog.title}'`, 'succes', 5)
-      )
-    } catch (error) {
-      dispatch(showNotification('failed to like blog:', 'error', 5))
-    }
-  }
-
-  const handleDelete = async id => {
-    const blogToDelete = blogs.find(b => b.id === id)
-
-    if (window.confirm(`Delete ${blogToDelete.title}`)) {
-      try {
-        await blogServices.remove(id)
-        setBlogs(blogs.filter(blog => blog.id !== id))
-        dispatch(showNotification('Blog deleted successfully', 'success', 5))
-      } catch (error) {
-        dispatch(showNotification('Failed to delete blog', 'error', 5))
-      }
-    }
   }
 
   return (
@@ -107,23 +51,11 @@ function App() {
           </Togglable>
           <h2>Blogs</h2>
           {blogs.map(blog => (
-            <Blog
-              key={blog.id}
-              blog={blog}
-              user={user}
-              handleLike={handleLike}
-              handleDelete={handleDelete}
-            />
+            <Blog key={blog.id} blog={blog} />
           ))}
         </div>
       ) : (
-        <LoginForm
-          handleSubmit={handleLogin}
-          handleUsernameChange={event => setUsername(event.target.value)}
-          handlePasswordChnage={event => setPassword(event.target.value)}
-          username={username}
-          password={password}
-        />
+        <LoginForm />
       )}
     </div>
   )
