113599bc6db302f6d7aae81a2442c1a3f4cefb0f
adanguerra1990 <aj90guerra@gmail.com>
2025-02-03 14:25:32 -0400

    Actualizar el componente Blogs para mostrar una tabla con los títulos y creadores de los blogs utilizando MUI y Tailwind CSS

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,17 +1,43 @@
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const Blogs = () => {
   const blogs = useSelector(state => state.blogs)
 
+  console.log('blog...', blogs)
+
   return (
     <div>
-      <h2>Blogs</h2>
-      {blogs.map(blog => (
-        <div key={blog.id}>
-          <Link to={`blogs/${blog.id}`}>{blog.title}</Link>
-        </div>
-      ))}
+      <h2 className='text-2xl text-center font-bold mb-4'>Blogs</h2>
+      <TableContainer component={Paper} className='shadow-md'>
+        <Table>
+          <TableHead>
+            <TableRow className='bg-gray-100'>
+              <TableCell className='font-bold'>Title</TableCell>
+              <TableCell className='font-bold'>Create by</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {blogs.map(blog => (
+              <TableRow key={blog.id} className='hover:bg-gray-50'>
+                <TableCell>
+                  <Link to={`blogs/${blog.id}`}>{blog.title}</Link>
+                </TableCell>
+                <TableCell>{blog.user ? blog.user.name : 'Unknown'}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   )
 }
