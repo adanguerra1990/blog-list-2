20ff780ab2e553e5094df4825d47b7476102ae03
adanguerra1990 <aj90guerra@gmail.com>
2025-02-05 09:44:01 -0400

    Mejorar el diseño del componente LoginForm utilizando clases de Tailwind CSS para una mejor presentación y usabilidad.

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -23,33 +23,42 @@ const LoginForm = () => {
   }
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username</label>
-          <input
-            type='text'
-            value={username}
-            name='username'
-            autoComplete='current-username'
-            onChange={handleChange}
-            disabled={loading}
-          />
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type='password'
-            value={password}
-            name='password'
-            autoComplete='current-password'
-            onChange={handleChange}
-            disabled={loading}
-          />
-        </div>
-        <button type='submit'>{loading ? 'Logging in...' : 'Login'}</button>
-      </form>
+    <div className='h-screen flex justify-center items-center vh-f'>
+      <div className='bg-white p-8 rounded-lg shadow-lg w-full max-w-md'>
+        <h2 className='text-2xl font-bold mb-6 text-gray-700'>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className='mb-4'>
+            <label className='block text-gray-700 mb-2'>Username</label>
+            <input
+              type='text'
+              value={username}
+              name='username'
+              autoComplete='current-username'
+              onChange={handleChange}
+              disabled={loading}
+              className='w-full p-2 border border-gray-300 rounded text-gray-800'
+            />
+          </div>
+          <div>
+            <label className='block text-gray-700 mb-2'>Password</label>
+            <input
+              type='password'
+              value={password}
+              name='password'
+              autoComplete='current-password'
+              onChange={handleChange}
+              disabled={loading}
+              className='w-full p-2 border border-gray-300 rounded text-gray-800'
+            />
+          </div>
+          <button
+            type='submit'
+            className='w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 mt-4 '
+          >
+            {loading ? 'Logging in...' : 'Login'}
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
