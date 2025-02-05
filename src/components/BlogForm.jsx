93a99e174601291785958d778c6d27e994620cb8
adanguerra1990 <aj90guerra@gmail.com>
2025-02-05 09:43:05 -0400

    Actualizar el componente BlogForm para incluir un botón de cancelar y mejorar el estilo de los campos de entrada utilizando clases de Tailwind CSS.

--- src/components/BlogForm.jsx
+++ src/components/BlogForm.jsx
@@ -3,7 +3,7 @@ import { showNotification } from '../redux/notificationReducer'
 import { createBlog } from '../redux/blogReducer'
 import { resetForm, setField } from '../redux/formReducer'
 
-const BlogForm = () => {
+const BlogForm = ({ onClose }) => {
   const dispatch = useDispatch()
   const { title, author, url } = useSelector(state => state.form)
   const user = useSelector(state => state.auth.user)
@@ -27,6 +27,7 @@ const BlogForm = () => {
           5
         )
       )
+      onClose()
     } catch (error) {
       dispatch(showNotification('failed to add blog:', 'error', 5))
     }
@@ -38,33 +39,58 @@ const BlogForm = () => {
     dispatch(setField({ field: name, value }))
   }
 
+  const handleCancel = () => {
+    dispatch(resetForm())
+    onClose()
+  }
+
   return (
     <div>
-      <h2>Create New</h2>
+      <h2 className='text-xl font-bold mb-4 text-gray-700'>Create New</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>Title</label>
+        <div className='mb-4'>
+          <label className='block text-gray-700 '>Title</label>
           <input
             type='text'
             name='title'
             value={title}
             onChange={handleChange}
+            className='text-gray-900  w-full p-2 border border-gray-300 rounded'
           />
         </div>
         <div>
-          <label>Author</label>
+          <label className='block text-gray-700 '>Author</label>
           <input
             type='text'
             name='author'
             value={author}
             onChange={handleChange}
+            className='text-gray-900  w-full p-2 border border-gray-300 rounded'
           />
         </div>
         <div>
-          <label>Url</label>
-          <input type='text' name='url' value={url} onChange={handleChange} />
+          <label className='block text-gray-700 '>Url</label>
+          <input
+            type='text'
+            name='url'
+            value={url}
+            onChange={handleChange}
+            className='text-gray-900  w-full p-2 border border-gray-300 rounded'
+          />
         </div>
-        <button type='submit'>Create</button>
+        <button
+          type='submit'
+          className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mt-4 mr-4'
+        >
+          Create
+        </button>
+        <button
+          type='submit'
+          className='bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-400 mt-4'
+          onClick={handleCancel}
+        >
+          Cancel
+        </button>
       </form>
     </div>
   )
