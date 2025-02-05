12fda795730c25ac0df36b9774734599e8974ecd
adanguerra1990 <aj90guerra@gmail.com>
2025-02-05 09:45:15 -0400

    Actualizar el componente Users para mejorar la presentación utilizando MUI y una tabla.

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -2,6 +2,15 @@ import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { initialUsers } from '../redux/usersReducer'
 import { Link } from 'react-router-dom'
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material'
 
 const Users = () => {
   const dispatch = useDispatch()
@@ -12,27 +21,29 @@ const Users = () => {
   }, [dispatch])
 
   return (
-    <div>
-      <h2>Users</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>User</th>
-            <th>Blogs Created</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => (
-            <tr key={user.id}>
-              <td>
-                <Link to={`/users/${user.id}`}>{user.name}</Link>
-              </td>
-              <td>{user.blogs.length}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <section className='container mx-auto'>
+      <h2 className='text-2xl text-center font-bold my-4'>Users</h2>
+      <TableContainer component={Paper} className='shadow-md'>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>User</TableCell>
+              <TableCell>Blogs Created</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map(user => (
+              <TableRow key={user.id} className='hover:bg-gray-50'>
+                <TableCell>
+                  <Link to={`/users/${user.id}`}>{user.name}</Link>
+                </TableCell>
+                <TableCell>{user.blogs.length}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </section>
   )
 }
 
